7dbd43d7eaa6f99caf39446b20fd4de720fa0789
Nken <nkentseur@gmail.com>
2023-02-16 20:56:03 +0000

    write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 
 /**
- * main - puts
- * Description: put the message in standar output
+ * main - write
+ * Description: write the message in standar output
  * Return: 0
  */
 int main(void)
 {
-	write(STDOUT_FILENO,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
