7610a517db8e7ff64574328036c0f61d83c469ba
Nken <nkentseur@gmail.com>
2023-02-16 20:32:43 +0000

    puts

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - printf
+ * Description: print the message in standar output
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
