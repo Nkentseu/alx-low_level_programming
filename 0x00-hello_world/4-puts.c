fcd29dea0b8178347fb3c6f124df741d91742555
Nken <nkentseur@gmail.com>
2023-02-16 20:28:14 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
+/**
  * main - puts
- *
- * return: 0
+ * Description: put the message in standar output
+ * Return: 0
  */
-int main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
