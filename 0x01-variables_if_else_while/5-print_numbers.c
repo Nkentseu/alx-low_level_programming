b3ced9e425485d2764009e80dde1c7daca96e63d
Nken <nkentseur@gmail.com>
2023-02-17 18:49:12 +0000

    number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Number
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 0;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar('0' + a);
+	}
+	putchar('\n');
+	return (0);
+}
