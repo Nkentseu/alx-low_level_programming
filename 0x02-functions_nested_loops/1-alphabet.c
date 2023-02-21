984612bdb6a3ad4afef86404337d36f2c3fa152c
Nken <nkentseur@gmail.com>
2023-02-21 08:32:18 +0000

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c++);
+	}
+	_putchar('\n');
+}
