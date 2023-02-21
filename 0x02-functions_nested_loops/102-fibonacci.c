4ef2d44409964d574c159e11efc9ec1fd7b07aa0
Nken <nkentseur@gmail.com>
2023-02-21 18:57:45 +0000

    fib1

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,14 +11,16 @@ int main(void)
 	unsigned long un_m1 = 0;
 	unsigned long n = 1;
 
-	for (n = 1; n <= 50; n++)
+	printf("%ld, ", un_p1);
+	while (n <= 50)
 	{
-		printf("%ld", un_p1);
-		if (n != 0)
-			printf(", ");
 		un_p1 = un + un_m1;
 		un_m1 = un;
 		un = un_p1;
+		printf("%ld", un_p1);
+		if (n < 50)
+			printf(", ");
+		n++;
 	}
 	printf("\n");
 	return (0);
