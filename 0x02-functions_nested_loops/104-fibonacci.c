b0d92fca5ec1ae9bc4ced43522eec2000d7fc110
Nken <nkentseur@gmail.com>
2023-02-22 06:35:37 +0000

    test

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,16 +7,39 @@
 int main(void)
 {
 	long double un = 1;
-	long double un_p1 = un;
-	long double un_m1 = 0;
-	unsigned long n = 1;
+	long double un_p1 = un, un_m1 = 0, p1 = 0, p2 = 0;
+	long double p1_1 = 0, p2_1 = 0, p1_2 = 0, p2_2 = 0;
+	int n = 1;
 
 	while (n <= 98)
 	{
 		un_p1 = un + un_m1;
 		un_m1 = un;
 		un = un_p1;
-		printf("%.0Lf", un_p1);
+		if (un + un_m1 > 51680708854858323072)
+		{
+			if (p1 == 0 || p2 == 0)
+			{
+				p1 = (un + un_m1) / 10000000000000000;
+				p2 = (un + un_m1) - (p1 * 10000000000000000);
+			}
+			else
+			{
+				p1 = (p1_1 + p1_2 + p2_1 + p2_2) / 10000000000000000;
+				p2 = (p1_1 + p1_2 + p2_1 + p2_2) - (p1 * 10000000000000000);
+			}
+			p1_2 = p1_1;
+			p2_2 = p2_1;
+			p1_1 = p1;
+			p2_1 = p2;
+			printf("%.0Lf%.0Lf", p1, p2);
+		}
+		else
+		{
+			printf("%.0Lf", un_p1);
+			un_m1 = un;
+			un = un_p1;
+		}
 		if (n < 98)
 			printf(", ");
 		n++;
