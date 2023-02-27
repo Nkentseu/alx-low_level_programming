f5b140e80572248fe569b949679087de960227f4
Nken <nkentseur@gmail.com>
2023-02-27 06:41:25 +0000

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * puts_half - put half
+ * @str: str
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	char end = '\n';
+	unsigned long begin = 0;
+
+	if (str == 0 || str[0] == '\0')
+		return;
+	while (str[begin++])
+		;
+	begin--;
+	if (begin <= 1)
+		return;
+	begin = (begin % 2 == 1) ? (begin / 2) + 1 : begin / 2;
+	while (str[begin])
+		write(1, &str[begin++], 1);
+	write(1, &end, 1);
+}
