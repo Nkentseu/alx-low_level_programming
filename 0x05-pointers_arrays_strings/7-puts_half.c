35fa3ddce8e44e462662b34b01f2e3e36f03197c
Nken <nkentseur@gmail.com>
2023-02-27 06:42:55 +0000

    half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,12 +12,18 @@ void puts_half(char *str)
 	unsigned long begin = 0;
 
 	if (str == 0 || str[0] == '\0')
+	{
+		write(1, &end, 1);
 		return;
+	}
 	while (str[begin++])
 		;
 	begin--;
 	if (begin <= 1)
+	{
+		write(1, &end, 1);
 		return;
+	}
 	begin = (begin % 2 == 1) ? (begin / 2) + 1 : begin / 2;
 	while (str[begin])
 		write(1, &str[begin++], 1);
