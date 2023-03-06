8ea574eac232ebbe97b6b4981f9d411e84544442
Nkentseu <nkentseu@gmail.com>
2023-03-06 12:05:53 +0000

    update

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -2,27 +2,6 @@
 #include <stdio.h>
 
 /**
- * copyto - copy src to dest
- * @dest: destination
- * @src: source
- * Return: pointer to dest
- */
-char *copyto(char *dest, char *src)
-{
-	unsigned int i = 0;
-
-	if (src == 0 || dest == 0)
-		return (0);
-	while (src[i] != '\0')
-	{
-		printf("ici\n");
-		dest[i] = src[i];
-		i++;
-	}
-	return (dest);
-}
-
-/**
  * set_string - set
  * @s: array of string
  * @to: string
@@ -30,7 +9,5 @@ char *copyto(char *dest, char *src)
  */
 void set_string(char **s, char *to)
 {
-	if (s == 0 || to == 0 || *s == 0)
-		return;
 	*s = to;
 }
