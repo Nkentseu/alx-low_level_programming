1dc3a9c80d2cfac2b1f41b1389c5e4d6af39ea0b
Nkentseu <nkentseu@gmail.com>
2023-03-06 12:03:24 +0000

    update

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -19,7 +19,7 @@ char *_strstr(char *haystack, char *needle)
 		{
 			unsigned int j = 0;
 
-			while (haystack[i + j] == needle[j])
+			while (haystack[i + j] != '\0' && haystack[i + j] == needle[j])
 				j++;
 			if (needle[j] == '\0')
 				return (&haystack[i]);
