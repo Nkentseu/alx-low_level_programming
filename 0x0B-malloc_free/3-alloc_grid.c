36f31cf7d1e5cbfdd73b4fb7da7e08811c201d4b
Nkentseu <nkentseu@gmail.com>
2023-03-14 05:51:42 +0000

    grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -63,7 +63,8 @@ int **alloc_grid(int width, int height)
 		output[i] = alloc_init(width, 0);
 		if (output[i] == 0)
 		{
-			free_last(output, 0, i);
+			free_last(output, 0, i - 1);
+			free(output);
 			return (0);
 		}
 	}
