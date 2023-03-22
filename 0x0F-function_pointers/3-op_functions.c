36040917756d2230b4c55188318f1a7fb2fa751f
Nkentseu <nkentseu@gmail.com>
2023-03-22 18:20:50 +0000

    op

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - addition
+ * @a: first value
+ * @b: second value
+ * Return: a + b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substraction
+ * @a: first value
+ * @b: second value
+ * Return: a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplication
+ * @a: first value
+ * @b: second value
+ * Return: a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division
+ * @a: first value
+ * @b: second value
+ * Return: a / b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo
+ * @a: first value
+ * @b: second value
+ * Return: a % b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
