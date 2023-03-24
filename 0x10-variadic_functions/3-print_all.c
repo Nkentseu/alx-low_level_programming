a40893631125db2088e5ea1c50e059c709a2dcd4
Nkentseu <nkentseu@gmail.com>
2023-03-24 03:09:30 +0000

    var

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -16,30 +16,29 @@ void print_all(const char * const format, ...)
 	va_start(args, format);
 	while (format != 0 && format[id] != '\0')
 	{
-		p = 0;
+		p = 1;
 		switch (format[id])
 		{
 			case 'c':
 				printf("%c", (char) va_arg(args, int));
-				p = 1;
 				break;
 			case 'i':
 				printf("%d", (int) va_arg(args, int));
-				p = 1;
 				break;
 			case 'f':
 				printf("%f", (float) va_arg(args, double));
-				p = 1;
 				break;
 			case 's':
-				s = va_arg(args, char *);
+				s = (char *) va_arg(args, char *);
 				if (s == NULL)
 				{
 					printf("(nil)");
 					break;
 				}
 				printf("%s", s);
-				p = 1;
+				break;
+			default:
+				p = 0;
 				break;
 		}
 		id++;
